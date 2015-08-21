13c4cb07326993fff267d29411f648d1f5cac53a
Simon <simon@simonemms.com>
2015-08-21 11:30:52 +0100

    Promisified the indexer. Changed from the deprecated ensure index method too

--- src/lib/indexer.js
+++ src/lib/indexer.js
@@ -12,54 +12,74 @@
 
 /* Third-party modules */
 var _ = require("lodash");
-var async = require("async");
+var bluebird = require("bluebird");
 
 
 /* Files */
 
 
+function callbackPromise (promise, cb) {
+
+    var usePromise = _.isFunction(cb) === false;
+
+    if (usePromise) {
+        return promise;
+    } else {
+        promise
+            .then(function () {
+                var args = _.toArray(arguments);
+
+                /* Create null error */
+                args.unshift(null);
+
+                /* Run the callback */
+                return cb.apply(null, args);
+
+            })
+            .catch(function (err) {
+                return cb(err);
+            });
+
+    }
+
+}
+
+
 module.exports.__factory = function $mongoIndexer () {
 
-    return function mongoIndexer (db, index, tableName, cb) {
+    return function mongoIndexer (db, tableName, index, opts, cb) {
+
+        if (_.isFunction(opts)) {
+            cb = opts;
+            opts = {};
+        }
+
+        if (_.isObject(opts) === false) {
+            opts = {};
+        }
+
+        var promise;
 
         if (_.isArray(index)) {
 
-            var returns = [];
-            async.eachSeries(index, function (obj, callback) {
+            promise = bluebird.all(index.map(function (obj) {
 
-                mongoIndexer(db, obj, tableName, function (err, result) {
+                return mongoIndexer(db, tableName, obj, opts);
 
-                    if (err) {
-                        callback(err);
-                        return;
-                    }
-
-                    returns.push(result);
-
-                    callback(null);
-
-                });
-
-            }, function (err) {
-
-                if (err) {
-                    cb(err);
-                    return;
-                }
-
-                cb(null, returns);
-
-            });
+            }));
 
         } else {
 
-            db.collection(tableName).ensureIndex(index, {
-                w: 1,
-                background: true,
-                name: Object.keys(index).join("-")
-            }, cb);
+            promise = db.collection(tableName)
+                .createIndex(index, _.defaults(opts, {
+                    w: 1,
+                    background: true,
+                    name: Object.keys(index).join("-")
+                }));
 
         }
+
+        return callbackPromise(promise, cb);
 
     };
 
