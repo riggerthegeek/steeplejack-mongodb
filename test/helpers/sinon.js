fc1a925d17e8a9b3cddddeefacd0ff37877ce3a3
Simon <simon@simonemms.com>
2015-08-21 15:00:24 +0100

    Added sinon and chai as promised deps

--- test/helpers/sinon.js
+++ test/helpers/sinon.js
@@ -12,9 +12,12 @@
 global.chai = require("chai");
 global.sinon = require("sinon");
 
+require("sinon-as-promised")(require("bluebird"));
+
 
 /* Files */
 
 
 chai.use(require("sinon-chai"));
+chai.use(require("chai-as-promised"));
 global.expect = chai.expect;
